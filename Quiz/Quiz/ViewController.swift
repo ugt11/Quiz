5acbbfe40dba8d8cad0e0c931fee8af9918a966e
spark-04 <>
2024-04-25 15:38:33 +0900

    init

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by spark-04 on 2024/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
